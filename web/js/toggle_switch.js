72cfeec68103ea7d8c9084f0ec4fbd3378687eb1
Luwin95 <b.aubin95@live.fr>
2017-04-14 16:14:38 +0200

    Ajout togglle modif background color

---
+++ web/js/toggle_switch.js
@@ -0,0 +1,27 @@
+$(function(){
+    var options = {
+        onText: "Sombre",
+        offText: "Claire",
+        onColor: 'darkgray',
+        offColor: 'default'
+    };
+
+    $('#toggle-switch').bootstrapSwitch(options);
+
+    $('#toggle-switch').on('switchChange.bootstrapSwitch', function (event, state) {
+        if(!state)
+        {
+            $('#page').css({'background-color':'#222222', 'color' : '#9B9D9D'});
+            $('.panel-heading').css({'background-color':'black', 'color' : '#9B9D9D', 'border-color': '#9B9D9D'});
+            $('.panel-body').css({'background-color':'#222222', 'color' : '#9B9D9D', 'border-color': '#9B9D9D'});
+        }
+        else{
+            $('#page').removeAttr('style');
+            $('.panel-heading').removeAttr('style');
+            $('.panel-body').removeAttr('style');
+        }
+    });
+});
+
+
+
